feea0f7dae9ae87011e9692215ab7841f5ae47ef
Shakhjakhon Komilov <97364147+shokybro77@users.noreply.github.com>
2023-05-12 23:34:10 +0500

    Dict

---
+++ dict.py
@@ -0,0 +1,23 @@
+car_0 = {
+    "model": "ferrari",
+    "color": "Red"
+}
+print(car_0["color"])
+
+en_uz = {
+    "apple": "olma",
+    "banana": "banan",
+    "strawberry": "qulupnay"
+}
+print(en_uz)
+
+# word = input("Input here: ")
+
+# if en_uz[word]:
+#     print(en_uz[word])
+# else:
+#     print("natog'ri")
+
+
+en_uz["cherry"] = "Gilos"
+print(en_uz)
